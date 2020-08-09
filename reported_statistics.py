e3cf07d150b75544aca97fe894a236e772fcc236
gdies <35563473+gdies@users.noreply.github.com>
2020-08-09 19:46:00 +0200

    Add files via upload

---
+++ reported_statistics.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def get_p(theta:np.ndarray, covariance_matrix:np.ndarray, sample_size:int=10000):
+    """
+    Calculate p-values for given parameters and their covariance matrix
+    Assume asymptotic normality
+    """
+    std_theta = np.sqrt(np.diag(covariance_matrix)) # standard deviation of beta
+    z = theta / std_theta # get z-statistics
+    normal_sample = np.random.normal(loc=0.0, scale=1.0, size=sample_size)
+    return np.array([np.where(normal_sample >= np.absolute(i), 1, 0).sum()/sample_size for i in z]) # p-values for each element in theta
