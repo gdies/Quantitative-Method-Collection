973e78f0777927a61263d9fdff58b35189407458
gdies <35563473+gdies@users.noreply.github.com>
2021-01-22 16:59:34 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../QMC/')) # add script directory to path
+sys.path.insert(0, os.path.abspath(r'../../QMC/')) # add script directory to path
 
 
 
